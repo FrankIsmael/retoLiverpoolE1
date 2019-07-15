7f3fb03f69f9fdfa760efb7c542aaa7274c5769d
Ismael <ismaelfcom93@gmail.com>
2019-07-15 11:20:36 -0500

    footer added

--- liverpool-front/src/components/Navbar.js
+++ liverpool-front/src/components/Navbar.js
@@ -16,11 +16,11 @@ class Navbar extends Component {
               <img src="https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/2c/b6/4f/2cb64f0f-8ad9-0642-06ca-8445e86dc4e6/AppIcon-0-1x_U007emarketing-0-0-85-220-0-4.png/246x0w.jpg" alt="liverpool" width="100" height="100" />
             </Link>
 
-            <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBurger" href="#">
+            <div role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBurger" href="#">
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
-            </a>
+            </div>
           </div>
 
           <div id="navbarBurger" className="navbar-menu">
